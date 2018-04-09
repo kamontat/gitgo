dcebcb541c622c9497d4f7c2f511f19210dd0b77
Kamontat Chantrachirathumrong <kamontat.c@hotmail.com>
2018-04-10 00:30:16 +0700

    [fix]: Fix initial commit nothing appear

--- commands/commit_initial.go
+++ commands/commit_initial.go
@@ -9,12 +9,16 @@ import (
 // AddCommitInital add command of 'commit initial'
 func AddCommitInital(emoji bool) cli.Command {
 	return cli.Command{
-		Name:    "initial",
-		Aliases: []string{"i", "init"},
+		Name:      "initial",
+		Aliases:   []string{"i", "init"},
+		Usage:     "Create default initial commit",
+		UsageText: "gitgo commit|cm|c init|i",
 		Action: func(c *cli.Context) error {
 			if emoji {
+				// fmt.Print("create as emoji")
 				client.MakeGitCommitWithEmoji(true, "🎉", "Initial commit")
 			} else {
+				// fmt.Print("create as test")
 				client.MakeGitCommitWithText(true, "init", "Initial commit")
 			}
 			return nil
