556081d2b09525f39fa69fdbf7b3df70e85ab040
Kamontat Chantrachirathumrong <kamontat.c@hotmail.com>
2018-08-16 11:38:39 +0700

    [doc] Update error message and godoc

--- model/commit.go
+++ model/commit.go
@@ -13,44 +13,61 @@ import (
 	survey "gopkg.in/AlecAivazis/survey.v1"
 )
 
+// CommitHeader is struct of Key and Value, using in list.yaml.
 type CommitHeader struct {
 	Key   string
 	Value string
 }
 
+// Format will return format of string.
 func (c *CommitHeader) Format() string {
 	return fmt.Sprintf("%-10s: %s", c.Key, c.Value)
 }
 
+// String will return string that show what is it.
 func (c *CommitHeader) String() string {
 	return fmt.Sprintf("commit key=%s, value=%s", c.Key, c.Value)
 }
 
+// Commit is Commit object of deal with commit things.
 type Commit struct {
 	repo *Repo
 	list []CommitHeader
 }
 
+// ListHeaderOptions return the list of string of commit that create by Format() method in CommitHeader.
 func (c *Commit) ListHeaderOptions() (list []string) {
 	for _, commits := range c.list {
 		list = append(list, commits.Format())
 	}
+	om.Log().ToVerbose("commit list", list)
+	if len(list) < 1 {
+		manager.
+			StartNewManageError().
+			AddNewErrorMessage("You must have at least 1 list.yaml files").
+			Throw().
+			ShowMessage(nil).
+			Exit()
+	}
 	return
 }
 
+// CommitMessage is the commit message for save in commit.
 type CommitMessage struct {
 	Key     string
 	Title   string
 	Message string
 }
 
+// GetKey will try to format the key to right way.
+// Otherwise, return normal Key
 func (c *CommitMessage) GetKey() string {
 	arr := strings.Split(c.Key, ":")
 	if len(arr) > 0 {
 		s := arr[0]
 		return strings.TrimSpace(s)
 	}
-	return ""
+	return c.Key
 }
 
 func (c *Commit) getQuestion() []*survey.Question {
@@ -88,6 +105,7 @@ func (c *Commit) getQuestion() []*survey.Question {
 	}
 }
 
+// LoadList will initial new list of Header.
 func (c *Commit) LoadList(vp *viper.Viper) *Commit {
 	if vp == nil {
 		om.Log().ToDebug("commit list", "viper is nil, cannot load list")
@@ -101,6 +119,7 @@ func (c *Commit) LoadList(vp *viper.Viper) *Commit {
 	return c.MergeList(vp)
 }
 
+// MergeList will merge current list to the new ones.
 func (c *Commit) MergeList(vp *viper.Viper) *Commit {
 	if vp == nil {
 		om.Log().ToVerbose("commit list", "viper is nil, cannot merge list")
@@ -117,6 +136,7 @@ func (c *Commit) MergeList(vp *viper.Viper) *Commit {
 	om.Log().ToVerbose("commit list", "merge commit list from "+vp.ConfigFileUsed())
 	for i, element := range vp.Get("list").([]interface{}) {
 		cm := element.(map[interface{}]interface{})
+
 		commitHeader := CommitHeader{
 			Key:   cm["key"].(string),
 			Value: cm["value"].(string),
@@ -129,6 +149,7 @@ func (c *Commit) MergeList(vp *viper.Viper) *Commit {
 	return c
 }
 
+// Commit is action for ask the message from user and call CustomCommit.
 func (c *Commit) Commit(hasMessage bool) {
 	// the questions to ask
 	var qs = c.getQuestion()
@@ -150,6 +171,7 @@ func (c *Commit) Commit(hasMessage bool) {
 	c.CustomCommit(answers)
 }
 
+// CustomCommit will run git commit -m "<message>" with the default format.
 func (c *Commit) CustomCommit(answers CommitMessage) {
 	var commitMessage string
 	if answers.Message == "" {
--- model/repo.go
+++ model/repo.go
@@ -7,12 +7,15 @@ import (
 	git "gopkg.in/src-d/go-git.v4"
 )
 
+// Repo is git repository object for gitgo.
 type Repo struct {
 	path       string
 	repo       *git.Repository
 	gitCommand *GitCommand
 }
 
+// NewRepo will return new repository with current path.
+// you must call setup for load git repository to memory.
 func NewRepo() *Repo {
 	err := manager.StartNewManageError()
 
@@ -22,6 +25,8 @@ func NewRepo() *Repo {
 	}
 }
 
+// Setup will load git repository to memory.
+// If any error occurred, exit with code 5.
 func (r *Repo) Setup() {
 	result, err := git.PlainOpen(r.path)
 	manager.ResetError().AddNewError(err).Throw().ShowMessage(nil).ExitWithCode(5)
@@ -29,12 +34,15 @@ func (r *Repo) Setup() {
 	r.repo = result
 }
 
+// Status will return *git.Status.
 func (r *Repo) Status() git.Status {
 	result, err := r.GetWorktree().Status()
 	manager.ResetError().AddNewError(err).Throw().ShowMessage(nil).ExitWithCode(5)
 	return result
 }
 
+// Add get array of filepath, and return ErrManager.
+// anyway, It's will run os.Exit with code 10 if any error occurred.
 func (r *Repo) Add(filepath []string) *manager.ErrManager {
 	exception := manager.StartNewManageError()
 	for _, f := range filepath {
@@ -45,22 +53,28 @@ func (r *Repo) Add(filepath []string) *manager.ErrManager {
 	return exception
 }
 
+// AddAll will run git add -A command in cli.
 func (r *Repo) AddAll() *manager.ErrManager {
 	return r.gitCommand.Exec("add", "-A")
 }
 
+// GetGitRepository will return git.Repository of this Repo
 func (r *Repo) GetGitRepository() *git.Repository {
 	return r.repo
 }
 
+// GetWorktree is getter method, which get git.Worktree from git.Repository.
+// It's will Exit with code 5 if any error occurred.
 func (r *Repo) GetWorktree() *git.Worktree {
 	repo := r.GetGitRepository()
 	result, err := repo.Worktree()
+
 	manager.ResetError().AddNewError(err).Throw().ShowMessage(nil).ExitWithCode(5)
 
 	return result
 }
 
+// GetCommit will return Commit object.
 func (r *Repo) GetCommit() *Commit {
 	return &Commit{
 		repo: r,
