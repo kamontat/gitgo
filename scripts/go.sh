2804acc248e4bf412a04f2af7df3b2d03e8617e4
Kamontat Chantrachirathumrong <developer@kamontat.net>
2021-03-19 12:32:54 +0700

    chore(script): add `./go.sh help` and `./go.sh all $1`

--- scripts/go.sh
+++ scripts/go.sh
@@ -20,20 +20,32 @@ command="$1"
 shift
 args=("$@")
 
-if [[ "$command" == "run" ]]; then
+if [[ "$command" == "help" ]]; then
+  echo """
+# ./scripts/go.sh [command] [...params]
+
+1. run          - called 'go run main.go'
+2. start        - called 'go build && ./cli'
+3. new \$1 \$2    - called 'go mod init \$2' on \$1 module
+4. all \$1       - called 'go \$1' on every modules
+5. publishLocal - called 'go build && cp cli /usr/local/bin/gitgo-next'
+6. raw \$@       - called 'go \$2' on \$1 module
+"""
+
+elif [[ "$command" == "run" ]]; then
   exec_go cli run main.go "${args[@]}" || exit $?
 elif [[ "$command" == "start" ]]; then
   exec_go cli build || exit $?
   "$PWD/cli/cli" "${args[@]}"
 elif [[ "$command" == "new" ]]; then
   exec_go "$1" mod init "$2"
-elif [[ "$command" == "build" ]]; then
-  exec_go cli build "${args[@]}" &&
-    exec_go core build "${args[@]}" &&
-    exec_go prompt build "${args[@]}" &&
-    exec_go git build "${args[@]}" &&
-    exec_go config build "${args[@]}" &&
-    exec_go utils build "${args[@]}" || exit $?
+elif [[ "$command" == "all" ]]; then
+  exec_go cli "${args[@]}" &&
+    exec_go core "${args[@]}" &&
+    exec_go prompt "${args[@]}" &&
+    exec_go git "${args[@]}" &&
+    exec_go config "${args[@]}" &&
+    exec_go utils "${args[@]}" || exit $?
 elif [[ "$command" == "publishLocal" ]]; then
   exec_go cli build || exit $?
   cp "$PWD/cli/cli" "/usr/local/bin/gitgo-next"
